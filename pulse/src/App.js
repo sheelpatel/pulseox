6192b5304ba5070b01795b5e3bd7737842702fbe
sheelpatel <sheelp@mit.edu>
2018-12-20 23:15:38 -0600

    updated landing page

--- pulse/src/App.js
+++ pulse/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './heart.png';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button variant="contained" size="large" color="#ff4400">
+          Start Diagnosis
+          </Button>
         </header>
       </div>
     );
