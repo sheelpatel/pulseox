e558b7695d041316d39e0ffb1717e8294c73e425
sheelpatel <sheelp@mit.edu>
2018-12-22 01:59:33 -0600

    Updates

---
+++ precache-manifest.86d316af2bd11d704318dcb025620123.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e451d53c350ccfa1bb",
+    "url": "/pulseox/static/js/runtime~main.a4e451d5.js"
+  },
+  {
+    "revision": "d35832de871f42930a04",
+    "url": "/pulseox/static/js/main.d35832de.chunk.js"
+  },
+  {
+    "revision": "2ec90c1eb194e1ef4e42",
+    "url": "/pulseox/static/js/1.2ec90c1e.chunk.js"
+  },
+  {
+    "revision": "d35832de871f42930a04",
+    "url": "/pulseox/static/css/main.b91f6953.chunk.css"
+  },
+  {
+    "revision": "b2f03d395be1af46e252f2bfd5ed0063",
+    "url": "/pulseox/index.html"
+  }
+];
